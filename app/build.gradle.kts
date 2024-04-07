653bf25e4e0e37cde57552faad63207df7084ec2
Daniel Lee <lee.daniel6@northeastern.edu>
2024-04-07 00:21:00 -0400

    Read user point amount from database to display max amount bet. Layout adjusted a little.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,6 @@ dependencies {
 
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-
-    // Add the dependency for the Realtime Database library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-database")
 
     // Add dependency for Line Chart and charting data for assets/coins
